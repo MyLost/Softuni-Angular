117833de33462ac83a6a242a24e0e31fd8ddb159
midas <ddimitrov10@gmail.com>
2024-04-28 10:06:46 +0300

    java backend

--- backend/src/main/java/npd21/softuni/backend/entities/Project.java
+++ backend/src/main/java/npd21/softuni/backend/entities/Project.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,9 +15,15 @@ import lombok.Setter;
 @Table(schema = "softuni")
 public class Project extends Base {
 
+    @NotEmpty(message = "name is required!")
     private String name;
+    @NotEmpty(message = "description is required!")
     private String description;
+    @NotEmpty(message = "url is required!")
+    @Pattern(regexp = "(([\\w]+:)?\\/\\/)?(([\\d\\w]|%[a-fA-f\\d]{2,2})+(:([\\d\\w]|%[a-fA-f\\d]{2,2})+)?@)?([\\d\\w][-\\d\\w]{0,253}[\\d\\w]\\.)+[\\w]{2,63}(:[\\d]+)?(\\/([-+_~.\\d\\w]|%[a-fA-f\\d]{2,2})*)*(\\?(&?([-+_~.\\d\\w]|%[a-fA-f\\d]{2,2})=?)*)?(#([-+_~.\\d\\w]|%[a-fA-f\\d]{2,2})*)?")
     private String url;
+    @NotEmpty(message = "picture url is required!")
+    @Pattern(regexp = "(([\\w]+:)?\\/\\/)?(([\\d\\w]|%[a-fA-f\\d]{2,2})+(:([\\d\\w]|%[a-fA-f\\d]{2,2})+)?@)?([\\d\\w][-\\d\\w]{0,253}[\\d\\w]\\.)+[\\w]{2,63}(:[\\d]+)?(\\/([-+_~.\\d\\w]|%[a-fA-f\\d]{2,2})*)*(\\?(&?([-+_~.\\d\\w]|%[a-fA-f\\d]{2,2})=?)*)?(#([-+_~.\\d\\w]|%[a-fA-f\\d]{2,2})*)?")
     private String pictureUrl;
     @ManyToOne(fetch = FetchType.EAGER)
     private User user;
