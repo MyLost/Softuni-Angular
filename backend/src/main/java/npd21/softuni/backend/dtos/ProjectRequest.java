84cbe861dede3c1924ed8b8959d6a4ef3b0d089d
midas <ddimitrov10@gmail.com>
2024-05-01 09:05:36 +0300

    java backend

--- backend/src/main/java/npd21/softuni/backend/dtos/ProjectRequest.java
+++ backend/src/main/java/npd21/softuni/backend/dtos/ProjectRequest.java
@@ -1,5 +1,7 @@
 package npd21.softuni.backend.dtos;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,8 +9,21 @@ import lombok.Setter;
 @Setter
 public class ProjectRequest {
 
+    private Long id;
+    @NotEmpty(message = "user id is required!")
+    private String userId;
+    @NotEmpty(message = "name is required!")
     private String name;
+    @NotEmpty(message = "description is required!")
     private String description;
+    @NotEmpty(message = "url is required!")
+    @Pattern(
+        regexp = "^(http:\\/\\/www\\.|https:\\/\\/www\\.|http:\\/\\/|https:\\/\\/)?[a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?$",
+        message = "url is not valid!")
     private String url;
+    @NotEmpty(message = "picture url is required!")
+    @Pattern(
+        regexp = "^(http:\\/\\/www\\.|https:\\/\\/www\\.|http:\\/\\/|https:\\/\\/)?[a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?$",
+        message = "url is not valid!")
     private String pictureUrl;
 }
