6ac791e51167835443734a0eee8b83b6524342f3
midas <ddimitrov10@gmail.com>
2024-05-04 12:53:23 +0300

    java backend

---
+++ backend/src/main/java/npd21/softuni/backend/dtos/ValidationResponse.java
@@ -0,0 +1,15 @@
+package npd21.softuni.backend.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+public class ValidationResponse {
+
+    private String message;
+}
