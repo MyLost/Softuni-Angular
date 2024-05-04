7dd8f3ba77ec0542a9595fe4080d9fbe786eb6df
midas <ddimitrov10@gmail.com>
2024-05-04 12:53:03 +0300

    java backend

---
+++ backend/src/main/java/npd21/softuni/backend/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package npd21.softuni.backend.repository;
+
+import npd21.softuni.backend.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findByEmail(String email);
+}
