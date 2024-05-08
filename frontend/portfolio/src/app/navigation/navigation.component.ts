115b073f32990cc46c9bfe74f0985acfcc9d32ae
midas <ddimitrov10@gmail.com>
2024-05-08 17:55:53 +0300

    frontend files

--- frontend/portfolio/src/app/navigation/navigation.component.ts
+++ frontend/portfolio/src/app/navigation/navigation.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { AuthenticationService } from '../auth/authentication.service';
 import {UserProfile} from "../register/register.component";
+import { MatToolbar } from "@angular/material/toolbar";
+import { MatIcon } from "@angular/material/icon";
+import { MatIconButton } from "@angular/material/button";
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [],
+  imports: [
+    MatToolbar,
+    MatIcon,
+    MatIconButton
+  ],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss'
 })
