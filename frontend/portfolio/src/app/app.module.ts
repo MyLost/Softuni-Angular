2ebee8157d6bf2f4366cd7a7516adedd4ea4f553
midas <ddimitrov10@gmail.com>
2024-05-19 10:09:02 +0300

    frontend files

--- frontend/portfolio/src/app/app.module.ts
+++ frontend/portfolio/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { Inject, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -10,23 +10,29 @@ import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HomeComponent } from "./home/home.component";
+import { AuthenticationService } from "./auth/authentication.service";
+import { authServiceFactory } from "./utils/utils";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NavigationComponent,
-    FooterComponent,
-    AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFirestoreModule,
-    NgbModule, // for firestore
-
-  ],
+    imports: [
+      BrowserModule,
+      AppRoutingModule,
+      NavigationComponent,
+      FooterComponent,
+      AngularFireModule.initializeApp(environment.firebaseConfig),
+      AngularFirestoreModule,
+      NgbModule,
+      HomeComponent,
+      // for firestore
+    ],
   providers: [
-    provideAnimationsAsync()
+    provideAnimationsAsync(),
+    { provide: 'AUTHENTICATION_PROVIDER', useValue: environment.authenticationService },
+    { provide: AuthenticationService, useFactory: authServiceFactory, deps: [ 'AUTHENTICATION_PROVIDER' ] }
   ],
   bootstrap: [AppComponent]
 })
