d027162cd86d1922c35eebddb44dffefcfc104c0
midas <ddimitrov10@gmail.com>
2024-05-19 10:09:52 +0300

    frontend files

--- frontend/portfolio/src/app/footer/footer.component.ts
+++ frontend/portfolio/src/app/footer/footer.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { DatePipe, NgClass } from "@angular/common";
+import { RouterLink } from "@angular/router";
 
 @Component({
-  selector: 'app-footer',
+  selector: 'np-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass,
+    RouterLink,
+    DatePipe
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
 
+  getPath() {
+    return '';
+  }
+
+  protected readonly test = module
 }
