3911b71a01fdca67d904bd6537d4cd5ce34d65ed
midas <ddimitrov10@gmail.com>
2024-05-23 15:40:07 +0300

    frontend files

--- frontend/portfolio/src/app/logout/logout.component.ts
+++ frontend/portfolio/src/app/logout/logout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseAuthenticationService } from "../auth/firebase.authentication.service";
 import { AuthenticationService } from "../auth/authentication.service";
 
 @Component({
