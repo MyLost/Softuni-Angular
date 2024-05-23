042b96d1eba037a3eff324a8805b41d6bbad0e23
midas <ddimitrov10@gmail.com>
2024-05-23 15:39:36 +0300

    frontend files

--- frontend/portfolio/src/app/home/home.component.ts
+++ frontend/portfolio/src/app/home/home.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatButton } from "@angular/material/button";
+import { MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardImage } from "@angular/material/card";
+import {MatCardModule} from '@angular/material/card';
+import { Project } from "../project/project.component";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButton,
+    MatCard,
+    MatCardHeader,
+    MatCardContent,
+    MatCardActions,
+    MatCardImage,
+    MatCardModule,
+    RouterLink
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  protected lastFiveProjects: Project[] = [];
+
+
+  ngOnInit(): void {
+      this.lastFiveProjects = [
+        {} as Project,
+        {} as Project,
+        {} as Project,
+        {} as Project,
+        {} as Project
+      ]
+  }
 }
