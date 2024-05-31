fdbe93423c9736e2d53cac63cfb669883160d7de
midas <ddimitrov10@gmail.com>
2024-05-31 09:57:58 +0300

    frontend files

--- frontend/portfolio/src/app/register/register.component.ts
+++ frontend/portfolio/src/app/register/register.component.ts
@@ -1,9 +1,15 @@
-import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
 import { RegisterService } from './register.service';
 import { UserAbout } from "../about/about.component";
+import { MatError, MatFormField, MatFormFieldModule, MatLabel } from "@angular/material/form-field";
+import { MatInput, MatInputModule } from "@angular/material/input";
+import { MatIcon } from "@angular/material/icon";
+import { provideNativeDateAdapter } from "@angular/material/core";
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatButton, MatIconButton } from "@angular/material/button";
+import { MatCheckbox } from "@angular/material/checkbox";
 
 export interface UserProfile {
   id: string;
@@ -24,15 +30,16 @@ export interface RegisterUser extends UserProfile {
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ FormsModule, JsonPipe ],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatIcon, MatIconButton, MatCheckbox, MatButton],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
-  providers: [ AngularFireDatabase ]
+  providers: [ AngularFireDatabase, provideNativeDateAdapter() ]
 })
 export class RegisterComponent {
 
   protected user: RegisterUser = {} as RegisterUser;
-  protected termsAndConditions: boolean = true;
+  protected termsAndConditions: boolean = false;
+  protected hide: boolean = true;
 
   constructor(private rSvc: RegisterService) {}
 
