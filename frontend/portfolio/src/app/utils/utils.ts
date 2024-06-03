79ec08204b1c191217e1991adff820fad1f6db7e
midas <ddimitrov10@gmail.com>
2024-06-03 15:16:51 +0300

    frontend files

--- frontend/portfolio/src/app/utils/utils.ts
+++ frontend/portfolio/src/app/utils/utils.ts
@@ -1,3 +1,19 @@
-import {BehaviorSubject} from "rxjs";
+import { BehaviorSubject } from "rxjs";
+import { FirebaseAuthenticationService } from "../auth/firebase.authentication.service";
+import { SpringAuthenticationService } from "../auth/spring.authentication.service";
+
+const authenticationProviderCnf: any = {
+  'firebase' : {
+    provider: () =>  new FirebaseAuthenticationService()
+  },
+  'spring' : {
+    provider: () =>  new SpringAuthenticationService()
+  }
+}
 
 export const loadingSub: BehaviorSubject<boolean> = new BehaviorSubject(false);
+
+export function authServiceFactory(authenticationProviderName: string) {
+  console.log(authenticationProviderCnf[authenticationProviderName].provider())
+  return authenticationProviderCnf[authenticationProviderName].provider();
+}
